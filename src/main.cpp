c836ba6df094f6d45f58aed60498504cc51a5904
Bgarcia117 <bryant.garcia.1771@gmail.com>
2025-07-02 21:13:42 -0500

    Add: Player constructor and game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,49 @@
 ﻿#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
-int main()
-{
-	std::cout << "Hello CMake." << std::endl;
-	return 0;
+class Player {
+public: 
+	Player()
+		: shape(sf::PrimitiveType::LineStrip, 4), position(500.f, 500.f), angle(45.f) {
+		shape[0].position = sf::Vector2f(0.f, -30.f);
+		shape[1].position = sf::Vector2f(-30.f, -30.f);
+		shape[2].position = sf::Vector2f(0.f, 15.f);
+		shape[3].position = sf::Vector2f(30.f, -30.f);
+
+		for (sf::Vertex& vertex : shape) {
+			vertex.color = sf::Color::Green;
+		}
+	}
+
+	void Draw(sf::RenderWindow& window) {
+		sf::Transform transform;
+		transform.translate(position).rotate(sf::radians(angle));
+		window.draw(shape, transform);
+	}
+
+private:
+	sf::VertexArray shape;
+	sf::Vector2f position;
+	float angle;
+	
+};
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(1200, 900), "Asteroids", sf::Style::Close | sf::Style::Titlebar);
+
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is < sf::Event::Closed) {
+				window.close();
+			}
+ 		}
+
+		// Update Game
+		window.clear();
+
+		// Draw Game
+		window.display();
+	}
 }
