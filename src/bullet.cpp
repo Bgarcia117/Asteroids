6dc657078e7776a876e8c49f3898b7ba6354b992
Bgarcia117 <bryant.garcia.1771@gmail.com>
2025-07-17 11:06:45 -0400

    Refactor: Moved transformation logic to its own function for better modularity

--- src/bullet.cpp
+++ src/bullet.cpp
@@ -2,6 +2,7 @@
 #include "bullet.h"
 #include "game.h"
 #include "asteroid.h"
+#include "physics.h"
 
 Bullet::Bullet(sf::Vector2f position, sf::Vector2f direction) :
 	Entity(position, 0.f), shape(1.f), direction(direction), lifetime(BULLET_LIFE) {
@@ -36,46 +37,10 @@ void Bullet::update(float deltaTime) {
 			transform.translate(asteroid->position);
 			transform.rotate(sf::degrees(asteroid->angle));
 
-			
-			size_t numOfVerticies = polygon.getVertexCount();
-			size_t intersectionCount = 0;
-
-			// Used for how long the ray starting at the bullet should be, wasn't used yet
-			sf::Vector2f rayEnd = sf::Vector2f(std::numeric_limits<float>::max(), position.y);
-
-			// Iterates over the vertex array
-			for (size_t j = 0; j < numOfVerticies; j++) {
-				// Treats the edge as a line
-				// Turns the verticies into world coordinates
-				sf::Vector2f p1 = transform.transformPoint(polygon[j].position);
-
-				// %numOfVerticies accounts for the edge case of the last one polygon[11] and connects
-				// it to the first vertex
-				sf::Vector2f p2 = transform.transformPoint(polygon[(j + 1) % numOfVerticies].position);
-
-				// Checks if the bullet would intersect if on the same level vertically
-				if ((p1.y < position.y && p2.y >= position.y) ||
-					(p2.y < position.y && p1.y >= position.y)) {
-
-					// "Interpolation" used to check where bullet would hit along asteroid edge
-					// Its a proportion, t=0 means its at p1, 1 = p2, and 0.5 is halfway
-					float t = (position.y - p1.y) / (p2.y - p1.y);
-
-					// Adds the t to find the edge x-axis would intersect with the bullet
-					// p2.x - p1.x gives you the distance between them
-					// Multiplying accounts for sloped edges
-					float intersectX = p1.x + t * (p2.x - p1.x);
-
-					// Checks if the asteroid intersects to the right and how many times
-					if (intersectX < position.x) {
-						intersectionCount++;
-					}
-				}
-			}
-
 			// Checks if the num  of intersections is even or odd
 			// Imagine a line drawn over the shape, how many times does it overlap?
-			if (intersectionCount % 2 == 1) {
+			if (physics::intersects(position,
+				physics::getTransformed(asteroid->getVertexArray(), transform))) {
 				lifetime = 0.f;
 			}
 		}
