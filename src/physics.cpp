357d65c78c7768cfd0b1eb755d7fe1863defbb4e
Bgarcia117 <bryant.garcia.1771@gmail.com>
2025-07-20 22:28:35 -0400

    Finished implementing SAT Algorithm for convex polygon collision detection

--- src/physics.cpp
+++ src/physics.cpp
@@ -1,4 +1,6 @@
 #include "physics.h"
+#include <iostream>
+#include <limits>
 
 bool physics::intersects(const sf::Vector2f& point, const sf::VertexArray& polygon) {
 	size_t numOfVerticies = polygon.getVertexCount();
@@ -43,12 +45,15 @@ bool physics::intersects(const sf::Vector2f& point, const sf::VertexArray& polyg
 }
 
 // Test Driver Function
-// Uses SAT: Separating Algorithm Theory
-// Only works on convex polygons
+// Uses the Separating Aaxis Theorem (SAT) algorithm
+// Only works on convex polygons, has issues with concave if they fit together like puzzle peices
 bool physics::intersects(const sf::VertexArray& poly1, const sf::VertexArray& poly2) {
-	size_t n1 = poly1.getVertexCount() - 1;
-	size_t n2 = poly1.getVertexCount() - 1;
 
+	// Get vertex counts of both polygons (Linestrip repeats first vertex to close shape
+	size_t n1 = poly1.getVertexCount() - 1;
+	size_t n2 = poly2.getVertexCount() - 1;
+
+	// Test all edge normals from polygon 1 as potential separting axes
 	for (size_t i = 0; i < n1; i++) {
 		sf::Vector2f edge = poly1[i].position - poly1[(i + 1) % n1].position;
 		sf::Vector2f normal(-edge.y, edge.x);
@@ -61,9 +66,27 @@ bool physics::intersects(const sf::VertexArray& poly1, const sf::VertexArray& po
 		float max1 = std::numeric_limits<float>::min();
 		float min2 = std::numeric_limits<float>::max();
 		float max2 = std::numeric_limits<float>::min();
+
+		for (size_t j = 0; j < n1; j++) {
+			float projection =
+				poly1[j].position.x * normal.x + poly1[j].position.y * normal.y;
+			min1 = std::min(min1, projection);
+			max1 = std::max(max1, projection);
+		}
+
+		for (size_t k = 0; k < n2; k++) {
+			float projection =
+				poly2[k].position.x * normal.x + poly2[k].position.y * normal.y;
+			min2 = std::min(min2, projection);
+			max2 = std::max(max2, projection);
+		}
+
+		if (max1 < min2 || max2 < min1) {
+			return false;
+		}
 	}
 
-	return false;
+	return true;
 }
 
 sf::VertexArray physics::getTransformed(const sf::VertexArray& polygon,
